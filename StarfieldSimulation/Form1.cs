34b287c098c767c84488662fd313767cd8442f39
Bekzat <75122487+bekzat@users.noreply.github.com>
2020-12-13 21:07:03 +0600

    upd

--- StarfieldSimulation/Form1.cs
+++ StarfieldSimulation/Form1.cs
@@ -12,9 +12,80 @@ namespace StarfieldSimulation
 {
     public partial class Form1 : Form
     {
+        public class Star
+        {
+            public float x { get; set; }
+            public float y { get; set; }
+            public float z { get; set; }
+        }
+
+        private Star[] stars = new Star[15000];
+
+        private Random random = new Random();
+
+        private Graphics graphics;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.Black);
+            foreach (var star in stars)
+            {
+                DrawStar(star);
+                MoveStar(star);
+            }
+
+            pictureBox1.Refresh();
+
+        }
+
+        private void MoveStar(Star star)
+        {
+            star.z -= 10;
+            if(star.z<1)
+            {
+                star.x = random.Next(-pictureBox1.Width, pictureBox1.Width);
+                star.y = random.Next(-pictureBox1.Height, pictureBox1.Height);
+                star.z = random.Next(1, pictureBox1.Width);
+            }
+        }
+
+        private void DrawStar(Star star)
+        {
+            float starSize = Map(star.z,0,pictureBox1.Width,7,0);
+
+            float x = Map(star.x / star.z, 0, 1, 0, pictureBox1.Width) + pictureBox1.Width / 2;
+
+            float y = Map(star.x / star.z, 0, 1, 0, pictureBox1.Height) + pictureBox1.Height / 2;
+
+            graphics.FillEllipse(Brushes.GreenYellow, x, y, starSize, starSize);
+        }
+
+        private float Map(float n, float start1, float stop1, float start2, float stop2)
+        {
+            return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            graphics = Graphics.FromImage(pictureBox1.Image);
+
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new Star()
+                {
+                    x = random.Next(-pictureBox1.Width, pictureBox1.Width),
+                    y = random.Next(-pictureBox1.Height, pictureBox1.Height),
+                    z = random.Next(1, pictureBox1.Width)
+                };
+            }
+            timer1.Start();
         }
     }
 }
